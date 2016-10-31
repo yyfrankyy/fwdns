1048e34b4c71751cb740756b04b4ab0551af8102
yyfrankyy <yyfrankyy@gmail.com>
2016-10-31 14:11:43 +0800

    feature: move mocha to tap

--- test/zone.js
+++ test/zone.js
@@ -1,41 +1,24 @@
-const assert = require('chai').assert;
+const tap = require('tap');
 const Zone = require('../zone');
 
 const DNS = '8.8.8.8';
+let zone = new Zone(DNS, ['qq.com']);;
+zone.buildIndex();
+tap.type(zone.root.com, 'object');
+tap.equal(zone.root.com.__, 1);
+tap.type(zone.root.com.qq, 'object');
+tap.equal(zone.root.com.qq.__, 0);
 
-describe('zone', () => {
-  describe('build index', () => {
-    it('can build a domain index', () => {
-      const zone = new Zone(DNS, ['qq.com']);;
-      zone.buildIndex();
-      assert.typeOf(zone.root.com, 'object');
-      assert.equal(zone.root.com.__, 1);
-      assert.typeOf(zone.root.com.qq, 'object');
-      assert.equal(zone.root.com.qq.__, 0);
-    });
-  });
-  describe('match', () => {
-    let zone;
-    before(() => {
-      zone = new Zone(DNS, ['qq.com', 'baidu.com', 'cn']);
-      zone.buildIndex();
-    });
-    it('exact match', () => {
-      assert(zone.match('qq.com'));
-      assert(zone.match('baidu.com'));
-      assert(zone.match('cn'));
-      assert.isNotOk(zone.match('notfound.com'));
-    });
-    it('suffix match', () => {
-      assert(zone.match('qq.cn'));
-      assert(zone.match('what.ever.qq.cn'));
-      assert(zone.match('www.baidu.com'));
-      assert(zone.match('www.qq.com'));
-      assert.isNotOk(zone.match('com'));
-      assert.isNotOk(zone.match('qqcn'));
-    });
-    it('whole match', () => {
-      assert.isNotOk(zone.match('www.qq.com', true));
-    });
-  })
-});
+zone = new Zone(DNS, ['qq.com', 'baidu.com', 'cn']);
+zone.buildIndex();
+tap.ok(zone.match('qq.com'));
+tap.ok(zone.match('baidu.com'));
+tap.ok(zone.match('cn'));
+tap.notOk(zone.match('notfound.com'));
+tap.ok(zone.match('qq.cn'));
+tap.ok(zone.match('what.ever.qq.cn'));
+tap.ok(zone.match('www.baidu.com'));
+tap.ok(zone.match('www.qq.com'));
+tap.notOk(zone.match('com'));
+tap.notOk(zone.match('qqcn'));
+tap.notOk(zone.match('www.qq.com', true));
